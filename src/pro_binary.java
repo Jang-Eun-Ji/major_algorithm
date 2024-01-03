8d9dec13f6a4bcc1b86bcc1c57fc1171a9860ea8
Jang-Eun-Ji <jej132456@gmail.com>
2024-01-03 18:02:24 +0900

    bin pro

--- src/pro_binary.java
+++ src/pro_binary.java
@@ -5,34 +5,18 @@ public class pro_binary {
     public static void main(String[] args) {
         int n = 6;
         int[] times = {7, 10};
-        int []complish_people = new int[times[times.length -1]*times[0]];
 
-        for (int i = 1; i < complish_people.length; i++) {
-            for (int j = 0; j < times.length; j++) {
-                complish_people[i] = times[j]*i;
-            }
+        int[] timeArr = new int[times[times.length -1] * n];
+        for (int i = 0; i < timeArr.length; i++) {
+            timeArr[i] = i;
         }
-
-
-        Arrays.sort(complish_people);
-        System.out.println(Arrays.toString(complish_people));
-    }
-
-    static int bin(int n, int[] times){
         int start = 0;
-        int last = times[times.length -1]*times[0]*n/2;
-        while (start <= last){
-            int middle = (start + last)/2;
-//            if(middle < xx){
-//                start = middle + 1;
-//            }
-//            if(middle > xx){
-//                last = middle -1;
-//            }
-//            if(middle == xx){
-//                return middle;
-//            }
+        int end = timeArr.length - 1;
+        while (start <= end){
+            int middle = (start + end)/2;
+            if(timeArr[middle] > )
         }
-        return -1;
+        if(timeArr )
     }
+    void
 }
--- src/pro_boat.java
+++ src/pro_boat.java
@@ -4,20 +4,17 @@ public class pro_boat {
     public static void main(String[] args) {
         int[] people = {70, 50, 80, 50};
         int limit = 100;
-        int count = people.length;
+        int count = 0;
+        int index = 0;
         Arrays.sort(people);
-
-        int people_left = people.length - 1;
-
-        while (people_left >= 0){
-            if(people[people_left] < limit) {
-                for (int i = people_left -1; i >= 0; i--) {
-                    if(limit - people[people_left] >= people[i]){
-                         count--;
-                    }
-                }
+        for (int i = people.length - 1; i >= index; i--) {
+            if (people[i] + people[index] <= limit) {
+                index++;
+                count++;
             }
-            people_left--;
+            else {
+                count ++;
+            }
         }
         System.out.println(count);
     }
