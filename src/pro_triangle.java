e89621fc5ca5b6932dc4788029b088c13ae73c0d
Jang-Eun-Ji <jej132456@gmail.com>
2024-01-05 17:50:33 +0900

    pro5

--- src/pro_triangle.java
+++ src/pro_triangle.java
@@ -5,13 +5,22 @@ public class pro_triangle {
         int last_len = triangle[triangle.length -1].length;
         int[] arr = new int[triangle[triangle.length -1].length];
         int biggest_temp = 0;
-        for (int i = last_len; i <= 0; i--) {
-            int big_temp = triangle[last_len][i];
-            if(triangle[last_len - 1][i] < triangle[last_len -1][i -1] && i!=0 && i !=last_len){
-                big_temp = triangle[last_len -1][i -1];
+        for (int i = 0; i < triangle[triangle.length -1].length; i++) {
+            Num(i, last_len);
+        }
+    }
+    static int Num(int k, int last_len){
+        for (int i = last_len -1; i >= 0; i--) {
+            int big_temp = k;
+            if(triangle[i-1][i] < triangle[i -1][i -1] && i!=0 && i !=last_len){
+                big_temp += triangle[last_len -1][i -1];
             }else {
-                big_temp = triangle[last_len - 1][i];
+                big_temp += triangle[last_len - 1][i];
             }
+            if(){// 다차면 biggest에 담기, 메소드 만들기?
+
+            }
+            return big_temp;
 
         }
     }
