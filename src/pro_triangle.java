ee8467aa317cb8126835480a9e60475221283376
JangEunJi <>
2024-01-08 09:07:43 +0900

    pro8

--- src/pro_triangle.java
+++ src/pro_triangle.java
@@ -1,28 +1,20 @@
 import java.util.*;
+
 public class pro_triangle {
-    static int[][] triangle = {{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
+
+
     public static void main(String[] args) {
-        int last_len = triangle[triangle.length -1].length;
-        int[] arr = new int[triangle[triangle.length -1].length];
-        int biggest_temp = 0;
-        for (int i = 0; i < triangle[triangle.length -1].length; i++) {
-            Num(i, last_len);
-        }
-    }
-    static int Num(int k, int last_len){
-        for (int i = last_len -1; i >= 0; i--) {
-            int big_temp = k;
-            if(triangle[i-1][i] < triangle[i -1][i -1] && i!=0 && i !=last_len){
-                big_temp += triangle[last_len -1][i -1];
-            }else {
-                big_temp += triangle[last_len - 1][i];
+        int[][] triangle = {{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
+        int[][] arr = new int [triangle.length][triangle.length];
+        arr[0][0] = triangle[0][0];
+        for (int i = 1; i < triangle.length; i++) {
+            arr[i][0] = triangle[i][0] + arr[i -1][0];
+            arr[i][i] = triangle[i][i] + arr[i -1][i -1];
+            for (int j = 1; j <= i; j++) {
+                arr[i][j] = Math.max(arr[i - 1][j], arr[i - 1][j - 1]) + triangle[i][j];
             }
-            if(){// 다차면 biggest에 담기, 메소드 만들기?
-
-            }
-            return big_temp;
-
         }
+        Arrays.sort(arr[arr.length -1]);
+        System.out.println(arr[arr.length-1][arr.length -1]);
     }
 }
-
